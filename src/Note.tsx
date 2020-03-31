7d31990b010107f4303f7080fd34a359d6b55e47
Fernanda Caramico <fernandacaramico@gmail.com>
2020-03-30 21:03:22 -0300

    first-commit

---
+++ src/Note.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Black = styled.button`
+  background: black;
+  width: 40px;
+  height: 130px;
+  position: absolute;
+  margin: 1px;
+  margin-left: -20px;
+
+  :active{
+    background: #333;
+  }
+`
+const White = styled.button`
+background: white;
+width: 60px;
+height: 200px;
+box-shadow: 2px 5px
+border: 1px solid black;
+margin: 1px;
+
+
+:active{
+  background: #eee;
+}
+`
+
+
+type Props = {
+  color: string;
+  note: string;
+  clickHandler: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const Note: React.FC<Props> = ({
+  color, note, clickHandler
+}) =>
+  color === 'white' ? (<White value={note} onClick={clickHandler} />)
+     :
+    ( <Black value={note} onClick={clickHandler} /> )
+
+  ;
+
+export default Note;
